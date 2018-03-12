4b0170372fe74b320ed7214dea1288bd59990a74
Ubuntu <zeeshan@zeahmed-nc12.juztu5vg2hcezat5roi1m3kcvb.ax.internal.cloudapp.net>
2018-03-12 00:17:06 +0000

    Network made configurable by adding a param in config file.

--- networks/bilstm_ctc_net.py
+++ networks/bilstm_ctc_net.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+from .common import label_error_rate, loss, model, optimizer
+
 
 def create_network(features, seq_len, num_classes, is_training):
 
@@ -47,31 +49,4 @@ def create_network(features, seq_len, num_classes, is_training):
 
     # Time major
     logits = tf.transpose(logits, (1, 0, 2))
-
     return logits
-
-
-def loss(logits, labels, seq_len):
-    return tf.reduce_mean(tf.nn.ctc_loss(labels, logits, seq_len))
-
-
-def model(logits, seq_len):
-    model, log_prob = tf.nn.ctc_beam_search_decoder(
-        logits, seq_len)  # tf.nn.ctc_greedy_decoder(logits, seq_len)
-    return model[0], log_prob
-
-
-def label_error_rate(model, labels):
-    # Label Error Rate
-    ler = tf.edit_distance(tf.cast(model, tf.int32), labels)
-    mean_ler = tf.reduce_mean(ler)
-    return mean_ler
-
-
-def optimizer(loss, learning_rate):
-    adam_opt = tf.train.AdamOptimizer(
-        learning_rate=learning_rate)  # .minimize(loss)
-    gradients, variables = zip(*adam_opt.compute_gradients(loss))
-    gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-    optimizer = adam_opt.apply_gradients(zip(gradients, variables))
-    return optimizer
--- networks/deepspeech.py
+++ networks/deepspeech.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+from .common import loss, model, label_error_rate, optimizer
 
 def variable_on_worker_level(name, shape, initializer):
     return tf.get_variable(name=name, shape=shape, initializer=initializer)
@@ -122,34 +123,3 @@ def create_network(features, seq_len, num_classes, is_training):
         layer_6, [-1, batch_x_shape[0], n_hidden_6], name="logits")
 
     return logits
-
-
-def loss(logits, labels, seq_len):
-    return tf.reduce_mean(tf.nn.ctc_loss(labels, logits, seq_len))
-
-
-def model(logits, seq_len):
-    model, log_prob = tf.nn.ctc_beam_search_decoder(
-        logits, seq_len)  # tf.nn.ctc_greedy_decoder(logits, seq_len)
-    return model[0], log_prob
-
-
-def label_error_rate(model, labels):
-    # Label Error Rate
-    ler = tf.edit_distance(tf.cast(model, tf.int32), labels)
-    mean_ler = tf.reduce_mean(ler)
-    return mean_ler
-
-
-def optimizer(loss, learning_rate):
-    adam_opt = tf.train.AdamOptimizer(
-        learning_rate=learning_rate)  # .minimize(loss)
-    gradients, variables = zip(*adam_opt.compute_gradients(loss))
-    gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-    optimizer = adam_opt.apply_gradients(zip(gradients, variables))
-    # mom_opt = tf.train.MomentumOptimizer(
-    #     learning_rate=learning_rate, momentum=0.9)
-    # gradients, variables = zip(*mom_opt.compute_gradients(loss))
-    # gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-    # optimizer = mom_opt.apply_gradients(zip(gradients, variables))
-    return optimizer
